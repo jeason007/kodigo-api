98b305eb4268586663419360ada73ce43ef0c439
jose artero <joseartero31@gmail.com>
2023-02-24 15:50:55 -0600

    integracion de envio de meil

---
+++ app/Mail/sendMail.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class sendMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $data;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Kodigo Empleabilidad Panel',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'mail.sendMail',
+            with: [
+                "estudiantes" => $this->data
+            ],
+        );
+        // return $this->view('view.mail.sendMail');
+        // return $this->from(env("MAIL_FROM_ADDRESS"),env("MAIL_FROM_NAME"))
+        //         ->view('view.mail.sendMail')
+        //         ->with(["estudiante", $this->data]);
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ resources/views/mail/sendMail.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href=".../css/app.css">
+    <script src=".../js/app.js"></script>
+    <title>Meil</title>
+</head>
+<body>
+    {{-- {{item.NombreEstudiante}} --}}
+    <h1 class="text-center">Kodigo Empleabilidad Panel Estudiantes</h1>
+    <table class="table" >
+        <thead>
+            <tr>
+                <th scope="col" style="margin-right: 10%">Estudiante</th>
+                <th scope="col" style="margin-right: 10%">Empresa</th>
+                <th scope="col" style="margin-right: 10%">Bootcamp</th>
+                <th scope="col" style="margin-right: 10%">Fecha Inicio</th>
+                <th scope="col" style="margin-right: 10%">Fecha Finalizacion</th>
+                <th scope="col" style="margin-right: 10%">Monto a cobrar</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($estudiantes as $item)
+                <tr>
+                    <td style="margin-right: 10%">{{$item->NombreEstudiante}}</td>
+                    <td style="margin-right: 10%">{{$item->Empresa}}</td>
+                    <td style="margin-right: 10%">{{$item->BootCamps}}</td>
+                    <td style="margin-right: 10%">{{$item->FechaInicio}}</td>
+                    <td style="margin-right: 10%">{{$item->FechaFinal}}</td>
+                    <td style="margin-right: 10%">{{$item->MontoCobrar}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    {{-- @foreach ($estudiantes as $item)
+        <p>Nombre estudiante: {{$item->NombreEstudiante}}</p>
+        <p>Bootcamp: {{$item->BootCamps}}</p>
+        <p>Empresa: {{$item->Empresa}}</p>
+        <p>Cobro: {{$item->MontoCobrar}}</p>
+        <br>
+    @endforeach --}}
+</body>
+</html>
